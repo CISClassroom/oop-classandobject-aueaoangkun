7efc2782f1ace5e394ef932fdeb7a15c4692db52
Fin <aueaoangkun@kkumail.com>
2020-01-03 16:05:30 +0700

    5

---
+++ src/com/company/Tree.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Tree {
+    int[] photosynthesis(int carbon , int water){
+        int Water_ = 0;
+        int oxygen_ = 0;
+        int Glucose_ = 0;
+
+
+
+
+        int[] result = {Water_ , oxygen_ ,Glucose_};
+
+        System.out.println(result);
+
+        return result;
+
+    }
+}
